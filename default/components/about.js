cdb40b76f3cd3fbdca2a2e3667e129c966dff465
Evan Plaice <evanplaice@gmail.com>
2016-02-01 14:44:29 -0800

    Add social to AboutComponent

--- default/components/about.js
+++ default/components/about.js
@@ -1,12 +1,13 @@
 import { Component, View } from 'angular2/core';
+import { DerpPipe } from '../../shared/pipes/derp';
 
 @Component({
   selector: 'about',
-  inputs: [ 'name', 'contact' ]
+  inputs: [ 'name', 'contact', 'social' ]
 })
 @View({
   template: `
-  <header>
+  <header style="float:left">
     <h1 *ngIf="name">{{ name }}</h1>
     <div id="contact">
       <div *ngIf="contact.email" class="email"><a href="mailto:{{ contact.email }}">{{ contact.email }}</a></div>
@@ -14,6 +15,11 @@ import { Component, View } from 'angular2/core';
       <div *ngIf="contact.website" class="website"><a href="{{ contact.website }}">{{ contact.website }}</a></div>
     </div>
   </header>
-  `
+  <div id="social" style="float:right;">
+    <div *ngFor="#profile of social | derp">{{ profile.network }}: <a href="{{ profile.url }}">{{ profile.user }}</a></div>
+  </div>
+  <div style="clear:both;"></div>
+  `,
+  pipes: [ DerpPipe ]
 })
 export class AboutComponent {}
