4afdd98bddb6d6ee8247dd2f6c4fc811047ce9fb
Evan Plaice <evanplaice@gmail.com>
2017-12-16 06:01:31 -0700

    Update Modern facade with new project-level paths

--- modern/modern.facade.js
+++ modern/modern.facade.js
@@ -1,24 +1,24 @@
 // modern directives
-import { AboutComponent } from '../default/components/about';
-import { InfoComponent } from '../default/components/info';
-import { EmploymentComponent } from '../default/components/employment';
-import { ProjectsComponent } from '../default/components/projects';
-import { SkillsComponent } from './components/skills';
-import { ServiceComponent } from '../default/components/service';
-import { EducationComponent } from '../default/components/education';
-import { SocialComponent } from '../default/components/social';
-import { RecognitionComponent } from '../default/components/recognition';
-import { WritingComponent } from '../default/components/writing';
-import { ReadingComponent } from '../default/components/reading';
-import { SpeakingComponent } from '../default/components/speaking';
-import { GovernanceComponent } from '../default/components/governance';
-import { LanguagesComponent } from '../default/components/languages';
-import { SamplesComponent } from '../default/components/samples';
-import { ReferencesComponent } from '../default/components/references';
-import { TestimonialsComponent } from '../default/components/testimonials';
-import { InterestsComponent } from '../default/components/interests';
-import { ExtracurricularComponent } from '../default/components/extracurricular';
-import { AffiliationsComponent } from '../default/components/affiliations';
+import { AboutComponent } from 'ng2-resume/default/components/about';
+import { InfoComponent } from 'ng2-resume/default/components/info';
+import { EmploymentComponent } from 'ng2-resume/default/components/employment';
+import { ProjectsComponent } from 'ng2-resume/default/components/projects';
+import { SkillsComponent } from 'ng2-resume/modern/skills';
+import { ServiceComponent } from 'ng2-resume/default/components/service';
+import { EducationComponent } from 'ng2-resume/default/components/education';
+import { SocialComponent } from 'ng2-resume/default/components/social';
+import { RecognitionComponent } from 'ng2-resume/default/components/recognition';
+import { WritingComponent } from 'ng2-resume/default/components/writing';
+import { ReadingComponent } from 'ng2-resume/default/components/reading';
+import { SpeakingComponent } from 'ng2-resume/default/components/speaking';
+import { GovernanceComponent } from 'ng2-resume/default/components/governance';
+import { LanguagesComponent } from 'ng2-resume/default/components/languages';
+import { SamplesComponent } from 'ng2-resume/default/components/samples';
+import { ReferencesComponent } from 'ng2-resume/default/components/references';
+import { TestimonialsComponent } from 'ng2-resume/default/components/testimonials';
+import { InterestsComponent } from 'ng2-resume/default/components/interests';
+import { ExtracurricularComponent } from 'ng2-resume/default/components/extracurricular';
+import { AffiliationsComponent } from 'ng2-resume/default/components/affiliations';
 
 export const MODERN_DIRECTIVES = [
   AboutComponent,
