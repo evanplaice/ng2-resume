e4a46a2cc0dcccca6631abf617dc654bb5e3eb7b
Evan Plaice <evanplaice@gmail.com>
2017-12-16 07:25:56 -0700

    Rename default.js to default.facade.js

---
+++ default/default.facade.js
@@ -0,0 +1,44 @@
+// default directives
+import { AboutComponent } from 'ng2-resume/components/about';
+import { InfoComponent } from 'ng2-resume/components/info';
+import { EmploymentComponent } from 'ng2-resume/components/employment';
+import { ProjectsComponent } from 'ng2-resume/components/projects';
+import { SkillsComponent } from 'ng2-resume/components/skills';
+import { ServiceComponent } from 'ng2-resume/components/service';
+import { EducationComponent } from 'ng2-resume/components/education';
+import { SocialComponent } from 'ng2-resume/components/social';
+import { RecognitionComponent } from 'ng2-resume/components/recognition';
+import { WritingComponent } from 'ng2-resume/components/writing';
+import { ReadingComponent } from 'ng2-resume/components/reading';
+import { SpeakingComponent } from 'ng2-resume/components/speaking';
+import { GovernanceComponent } from 'ng2-resume/components/governance';
+import { LanguagesComponent } from 'ng2-resume/components/languages';
+import { SamplesComponent } from 'ng2-resume/components/samples';
+import { ReferencesComponent } from 'ng2-resume/components/references';
+import { TestimonialsComponent } from 'ng2-resume/components/testimonials';
+import { InterestsComponent } from 'ng2-resume/components/interests';
+import { ExtracurricularComponent } from 'ng2-resume/components/extracurricular';
+import { AffiliationsComponent } from 'ng2-resume/components/affiliations';
+
+export const DEFAULT_DIRECTIVES = [
+  AboutComponent,
+  InfoComponent,
+  EmploymentComponent,
+  ProjectsComponent,
+  SkillsComponent,
+  ServiceComponent,
+  EducationComponent,
+  SocialComponent,
+  RecognitionComponent,
+  WritingComponent,
+  ReadingComponent,
+  SpeakingComponent,
+  GovernanceComponent,
+  LanguagesComponent,
+  SamplesComponent,
+  ReferencesComponent,
+  TestimonialsComponent,
+  InterestsComponent,
+  ExtracurricularComponent,
+  AffiliationsComponent
+];
