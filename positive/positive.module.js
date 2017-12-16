3006a9f41346b8c6859fc380ad0d752685fda777
Evan Plaice <evanplaice@gmail.com>
2017-12-16 07:24:44 -0700

    Update positive module to use SHARED_DECLARATIONS

--- positive/positive.module.js
+++ positive/positive.module.js
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PositiveComponent } from 'ng2-resume/positive/positive';
-import { TEMPLATE_DIRECTIVES, TEMPLATE_PIPES } from 'ng2-resume/shared/shared';
+import { SHARED_DECLARATIONS } from 'ng2-resume/shared/shared.facade';
 import { POSITIVE_DECLARATIONS } from 'ng2-resume/positive/positive.facade';
 
 import { ResumeService } from 'ng2-resume/shared/services/resume';
@@ -13,8 +13,7 @@ import { ResumeService } from 'ng2-resume/shared/services/resume';
   ],
   declarations: [
     PositiveComponent,
-    TEMPLATE_DIRECTIVES,
-    TEMPLATE_PIPES,
+    SHARED_DECLARATIONS,
     POSITIVE_DECLARATIONS
   ],
   exports: [
