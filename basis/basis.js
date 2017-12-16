26e2e5259bd9d85b8f06f3271d3e6500598a5c98
Evan Plaice <evanplaice@gmail.com>
2017-12-16 06:35:32 -0700

    Remove deprecated code from basis.js

--- basis/basis.js
+++ basis/basis.js
@@ -1,5 +1,4 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { BASIS_DIRECTIVES } from '../basis';
 
 import template from './basis.html!text';
 import style from './basis.css!text';
@@ -9,11 +8,8 @@ import style from './basis.css!text';
   inputs: [ 'resume' ],
   template: template,
   style: [ style ],
-  directives: [ BASIS_DIRECTIVES ],
   encapsulation: ViewEncapsulation.Native
 })
 export class BasisComponent {
-  constructor () {
-    // console.log('resume');
-  }
+  constructor () { }
 }
