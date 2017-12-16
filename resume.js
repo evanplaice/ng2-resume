87ef55595e2c2a9a4be94f9b90613503f7021e53
Evan Plaice <evanplaice@gmail.com>
2017-12-16 08:54:47 -0700

    Refactor top-level exports

--- resume.js
+++ resume.js
@@ -1,15 +1,8 @@
-// -------------- FRESH themes --------------
+// ----------------- Modules -----------------
 export { ResumeModule as BasisModule } from 'ng2-resume/basis/basis.module';
 export { ResumeModule as CompactModule } from 'ng2-resume/compact/compact.module';
 export { ResumeModule as ModernModule } from 'ng2-resume/modern/modern.module';
 export { ResumeModule as PositiveModule } from 'ng2-resume/positive/positive.module';
 
-// -------------- FRESH models --------------
-import { TEMPLATE_MODELS } from './shared/shared';
-
-export const FRESH_MODELS = TEMPLATE_MODELS;
-
-// -------------- FRESH service --------------
-import { ResumeService as Service } from './shared/services/resume';
-
-export const ResumeService = Service;
+// ----------------- Services ----------------
+export { ResumeService } from 'ng2-resume/shared/services/resume';
