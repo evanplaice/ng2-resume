0d94d39f3f56eb3046fc57d68c6cfa1ca62442e8
Evan Plaice <evanplaice@gmail.com>
2017-12-16 07:55:17 -0700

    Refactor modern.facade.js to use the new default facade

--- modern/modern.facade.js
+++ modern/modern.facade.js
@@ -1,24 +1,25 @@
-// modern directives
-import { AboutComponent } from 'ng2-resume/default/components/about';
-import { InfoComponent } from 'ng2-resume/default/components/info';
-import { EmploymentComponent } from 'ng2-resume/default/components/employment';
-import { ProjectsComponent } from 'ng2-resume/default/components/projects';
 import { SkillsComponent } from 'ng2-resume/modern/skills';
-import { ServiceComponent } from 'ng2-resume/default/components/service';
-import { EducationComponent } from 'ng2-resume/default/components/education';
-import { SocialComponent } from 'ng2-resume/default/components/social';
-import { RecognitionComponent } from 'ng2-resume/default/components/recognition';
-import { WritingComponent } from 'ng2-resume/default/components/writing';
-import { ReadingComponent } from 'ng2-resume/default/components/reading';
-import { SpeakingComponent } from 'ng2-resume/default/components/speaking';
-import { GovernanceComponent } from 'ng2-resume/default/components/governance';
-import { LanguagesComponent } from 'ng2-resume/default/components/languages';
-import { SamplesComponent } from 'ng2-resume/default/components/samples';
-import { ReferencesComponent } from 'ng2-resume/default/components/references';
-import { TestimonialsComponent } from 'ng2-resume/default/components/testimonials';
-import { InterestsComponent } from 'ng2-resume/default/components/interests';
-import { ExtracurricularComponent } from 'ng2-resume/default/components/extracurricular';
-import { AffiliationsComponent } from 'ng2-resume/default/components/affiliations';
+import {
+  AboutComponent,
+  InfoComponent,
+  EmploymentComponent,
+  ProjectsComponent,
+  ServiceComponent,
+  EducationComponent,
+  SocialComponent,
+  RecognitionComponent,
+  WritingComponent,
+  ReadingComponent,
+  SpeakingComponent,
+  GovernanceComponent,
+  LanguagesComponent,
+  SamplesComponent,
+  ReferencesComponent,
+  TestimonialsComponent,
+  InterestsComponent,
+  ExtracurricularComponent,
+  AffiliationsComponent
+} from 'ng2-resume/default/default.facade';
 
 const DIRECTIVES = [
   AboutComponent,
